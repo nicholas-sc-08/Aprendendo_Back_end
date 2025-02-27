8661910d7ddf437bc4e41f5a9e71afe8ac9e19c2
NICHOLAS SERENCOVICH CARVALHO <nicholas_carvalho@estudante.sesisenai.org.br>
2025-02-27 17:20:41 -0300

    Fazendo Back_End

--- B-4/db.js
+++ B-4/db.js
@@ -1,6 +1,40 @@
 async function connect() {
 
+    if(global.connection){
+
+        return global.connection.connect();
+    };
+
     const { Pool } = require(`pg`);
     const pool = new Pool({connectionString: process.env.CONNECTION_STRING}); //ler dados com a conexão com o banco
-    const client = await pool.connect();
+    const cliente = await pool.connect();
+
+    const res = await cliente.query(`select now()`);
+    console.log(res.rows[0]);
+    
+    cliente.release();
+    global.connetion = pool;
+    
+    return pool.connect();
+};
+connect();
+
+//listagem de usuarios
+
+async function selecionar_usuarios(){
+
+    try {
+        
+        const cliente = await connect;
+        const res = await cliente.query(`SELECT * FROM USUARIOS`);
+        return res.rows;
+
+    } catch (error) {
+        
+    }
+};
+
+module.exports = {
+
+    selecionar_usuarios
 };
--- B-4/script.js
+++ B-4/script.js
@@ -4,12 +4,21 @@ const body_parser = require(`body-parser`);
 const pg = require(`pg`);
 
 require("dotenv").config();  // carrega as variaveis do arquivo .env
-const port = process.env.PORT; 
+const port = process.env.PORT; // armazena o número da porta
 
 const app = express();
+const db = require(`./db`);
+
 
 app.use(body_parser.json());
 app.use(cors());
+
+
+app.get(`/USUARIOS`, async (req, res) => {
+
+    const usuarios = await db.selecionar_usuarios();
+    res.json(usuarios);
+});
 
 app.get(`/`, async (req, res) => {
 
