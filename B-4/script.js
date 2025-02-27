ab7cc60686135704441659989ad247827dda1dc2
Nicholas Serencovich Carvalho <153335120+nicholas-sc-08@users.noreply.github.com>
2025-02-27 17:22:15 -0300

    Fazendo Back_end

--- B-4/script.js
+++ B-4/script.js
@@ -1,1 +1,35 @@
+const express = require(`express`);
+const cors = require(`cors`);
+const body_parser = require(`body-parser`);
+const pg = require(`pg`);
 
+require("dotenv").config();  // carrega as variaveis do arquivo .env
+const port = process.env.PORT; // armazena o número da porta
+
+const app = express();
+const db = require(`./db`);
+
+
+app.use(body_parser.json());
+app.use(cors());
+
+
+app.get(`/USUARIOS`, async (req, res) => {
+
+    const usuarios = await db.selecionar_usuarios();
+    res.json(usuarios);
+});
+ 
+app.get(`/`, async (req, res) => {
+
+    try {
+
+        res.json();
+
+    } catch (erro) {
+      
+        console.error(erro);
+    };
+});
+
+app.listen(port, () => console.log(`Servidor HTTP rodando na porta ${port}`));
